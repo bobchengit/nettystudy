cf2b12c0b7a45612f6aeade5325411af89df741a
bobchengit <hi_changzhou@126.com>
2016-10-20 14:58:11 +0800

    netty分包粘包

---
+++ nettystudy/src/main/java/com/sdnware/j2se/netty/$2/ChildChannelHandler.java
@@ -0,0 +1,14 @@
+package com.sdnware.j2se.netty.$2;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel ch) throws Exception {
+		
+		ch.pipeline().addLast(new TimeServerHandler());
+		
+	}
+}
