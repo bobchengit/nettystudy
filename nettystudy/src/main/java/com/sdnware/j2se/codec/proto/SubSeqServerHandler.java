4a3bcf3d097fce8153aa0ecbef444a9e89b60c5f
bobchengit <hi_changzhou@126.com>
2016-10-20 20:52:49 +0800

    服务端Proto

---
+++ nettystudy/src/main/java/com/sdnware/j2se/codec/proto/SubSeqServerHandler.java
@@ -0,0 +1,33 @@
+package com.sdnware.j2se.codec.proto;
+
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class SubSeqServerHandler extends ChannelHandlerAdapter {
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		SubscribeReqProto.SubscribeReq req = (SubscribeReqProto.SubscribeReq)msg;
+		System.out.println(req.toString());
+		ctx.writeAndFlush(resp(req.getSubReqID()));
+	}
+
+	private SubscribeRespProto.SubscribeResp resp(int subReqID) {
+		SubscribeRespProto.SubscribeResp.Builder resp = SubscribeRespProto.SubscribeResp.newBuilder();
+		resp.setSubReqID(subReqID);
+		resp.setRespCode(0);
+		resp.setDesc("Heee");
+		return resp.build();
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+
+	
+}
